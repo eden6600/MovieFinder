6a1e96b96c1d30ea72b5fb59c1051ebf102489a8
עדן חסן <edenhasan@gmail.com>
2018-12-09 19:42:46 +0200

    Updates

--- precache-manifest.6075010b08eb6f7f535476cb3cbba313.js
+++ precache-manifest.6075010b08eb6f7f535476cb3cbba313.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/MovieFinder/static/js/runtime~main.77015081.js"
   },
   {
-    "revision": "c2d7049ed10c292466cd",
-    "url": "/MovieFinder/static/js/main.c2d7049e.chunk.js"
+    "revision": "7fd59df2b79f07571eef",
+    "url": "/MovieFinder/static/js/main.7fd59df2.chunk.js"
   },
   {
     "revision": "decdfa56a612867257aa",
     "url": "/MovieFinder/static/js/1.decdfa56.chunk.js"
   },
   {
-    "revision": "c2d7049ed10c292466cd",
+    "revision": "7fd59df2b79f07571eef",
     "url": "/MovieFinder/static/css/main.a8fefebd.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/MovieFinder/static/css/1.16c488e5.chunk.css"
   },
   {
-    "revision": "a17b56ce5eb89029ff05080281ce5091",
+    "revision": "7cf0e3c7ca3c999524d6865cddd27530",
     "url": "/MovieFinder/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MovieFinder/precache-manifest.f83631293be09bea2621c632ea772954.js"
+  "/MovieFinder/precache-manifest.6075010b08eb6f7f535476cb3cbba313.js"
 );
 
 workbox.clientsClaim();
